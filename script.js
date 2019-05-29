0c8b34f476771a109954abd06530696dba6250ab
Isabella Boisen <isabellaboisen@Isabellas-MacBook-Pro.local>
2019-05-29 12:33:44 +0200

    alle sider er lavet

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+        document.querySelector("section").classList = "";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+        document.querySelector("section").classList = "hidden";
+
+    }
+}
